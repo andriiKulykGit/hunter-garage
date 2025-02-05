0ebced7620b954ecb9b2c9074687522a2dab2385
Andrii <andrii.kulyk.job@gmail.com>
2025-02-05 20:05:41 +0100

    Added check for some elms in JS

--- src/js/modules/tables.js
+++ src/js/modules/tables.js
@@ -1,21 +1,25 @@
-document.querySelectorAll(".accordion__link").forEach(btn => {
-	btn.addEventListener("click", e => {
-		e.preventDefault
+if (document.querySelectorAll(".accordion__link")) {
+	document.querySelectorAll(".accordion__link").forEach(btn => {
+		btn.addEventListener("click", e => {
+			e.preventDefault
 
-		hideTables()
+			hideTables()
 
-		const targetID = btn.getAttribute("aria-controls")
-		document.getElementById(targetID).removeAttribute("hidden")
-		document.querySelector(".search__content").classList.add("_active")
-		document.querySelector(".search__aside").classList.add("_hidden")
+			const targetID = btn.getAttribute("aria-controls")
+			document.getElementById(targetID).removeAttribute("hidden")
+			document.querySelector(".search__content").classList.add("_active")
+			document.querySelector(".search__aside").classList.add("_hidden")
+		})
 	})
-})
+}
 
-document.querySelector(".search__back").addEventListener("click", () => {
-	hideTables()
-	document.querySelector(".search__content").classList.remove("_active")
-	document.querySelector(".search__aside").classList.remove("_hidden")
-})
+if (document.querySelector(".search__back")) {
+	document.querySelector(".search__back").addEventListener("click", () => {
+		hideTables()
+		document.querySelector(".search__content").classList.remove("_active")
+		document.querySelector(".search__aside").classList.remove("_hidden")
+	})
+}
 
 function hideTables() {
 	document.querySelectorAll(".table").forEach(table => {
